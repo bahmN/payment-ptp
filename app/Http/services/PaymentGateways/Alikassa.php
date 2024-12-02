c18a4756ff81c7a822ee4354f8f6022945b8cd41
bahmN <iwanaev23@gmail.com>
2024-12-02 14:42:49 +0300

    Alikassa

--- app/Http/services/PaymentGateways/Alikassa.php
+++ app/Http/services/PaymentGateways/Alikassa.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Services\PaymentGateways;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
 class Alikassa {
+    const DIGISELLER_API_CALLBACK_URI = 'https://digiseller.market/callback/api';
+
     public function paymentLink($data) {
         $body = [
-            'amount' => round($data->amount, 1),
+            'amount' => round($data->amount, 2),
             'order_id' => (string) $data->invoice_id,
             'service' => config('alikassa.service')[$data->payment_id],
             'desc' => 'Оплата заказа №' . $data->invoice_id,
@@ -39,7 +42,6 @@ class Alikassa {
 
         if (null !== $response->json('url')) {
             Log::info('УСПЕХ. Оплата через Alikassa.', [$response->json()]);
-
             return $response->json('url');
         }
 
@@ -49,28 +51,66 @@ class Alikassa {
     }
 
     public function callback(Request $request) {
-        Log::info('AlikassaCallback.', [$request->all()]);
+        if (isset($request->payment_status) ?? $request->payment_status == 'paid') {
+            $verify = openssl_verify(
+                json_encode([
+                    'type' => $request->type,
+                    'id' => (int) $request->id,
+                    'order_id' => $request->order_id,
+                    'payment_status' => $request->payment_status,
+                    'amount' => $request->amount,
+                    'payment_amount' => $request->payment_amount,
+                    'is_partial_payment' => $request->is_partial_payment,
+                    'account' => $request->account,
+                    'service' => $request->service,
+                    'desc' => $request->desc,
+                ]),
+                base64_decode($request->sign),
+                Storage::disk('local')->get('alikassa/notification/public.pem'),
+            );
 
-        $verify = openssl_verify(
-            json_encode([
-                'type' => $request->type,
-                'id' => (int) $request->id,
-                'order_id' => $request->order_id,
-                'payment_status' => $request->payment_status,
-                'amount' => $request->amount,
-                'payment_amount' => $request->payment_amount,
-                'is_partial_payment' => $request->is_partial_payment,
-                'account' => $request->account,
-                'service' => $request->service,
-                'desc' => $request->desc,
-            ]),
-            base64_decode($request->sign),
-            Storage::disk('local')->get('alikassa/notification/public.pem'),
-        );
+            if ($verify) {
+                Order::where('invoice_id', $request->order_id)
+                    ->where('amount', round($request->original_amount, 2))
+                    ->update([
+                        'status' => 'P',
+                        'operation_id' => $request->id
+                    ]);
 
-        Log::info('AlikassaCallback.', ['verify' => $verify]);
+                $signData = [
+                    'invoice_id' => $request->order_id,
+                    'amount' =>  number_format($request->amount, 2, '.', ''),
+                    'currency' => 'RUB',
+                    'status' => 'paid'
+                ];
 
-        if ($verify) {
+                ksort($signData);
+
+                $stringToSign = '';
+                foreach ($signData as $key => $value) {
+                    $stringToSign .= "$key:$value;";
+                }
+
+                $signature = hash_hmac('SHA256', $stringToSign, config('digiseller.callback_key'), false);
+
+                $body = [
+                    'invoice_id' => $request->order_id,
+                    'amount' =>  number_format($request->amount, 2, '.', ''),
+                    'currency' => 'RUB',
+                    'status' => 'paid',
+                    'signature' => strtoupper($signature),
+                ];
+
+                $response = Http::asForm()->get(self::DIGISELLER_API_CALLBACK_URI, $body);
+
+                Log::info('Alikassa Callback.', ['Параметры Alikassa' => $request->all(), 'Ответ Digiseller' => $response->body()]);
+
+                return response()->json('200', 200);
+            }
+
+            return response()->json('Bad signature', 400);
         }
+
+        return response()->json('Bad status', 400);
     }
 }
